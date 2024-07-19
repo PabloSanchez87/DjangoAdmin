45d17deeeac351454b97c5de7cd9e7ce9ad0caf5
PabloSanchez <sancheztorrespablo@gmail.com>
2024-07-19 20:26:56 +0200

    Trabajamos con parámetros y contexto tambien con libros

--- books/urls.py
+++ books/urls.py
@@ -7,6 +7,7 @@ from .views import (
     editoriales_view,
     autores_view, 
     autor_detail,
+    libro_detail,
     libros_view,
 )
 
@@ -15,12 +16,12 @@ app_name="books"
 
 urlpatterns = [
     path("editoriales/", editoriales_view, name="editorial_list"),  
+   
     path("autores/", autores_view, name="autor_list"),  
-    
-    # Solo machea con autores/# siendo # un número.
-    path("autores/<int:id>/", autor_detail, name="autor_detail"),  
+    path("autores/<int:id>/", autor_detail, name="autor_detail"),  # Solo machea con autores/# siendo # un número.
     
     path("libros/", libros_view, name="libro_list"),   
+    path("libros/<int:id>/", libro_detail, name="libro_detail"),
 ]
 
     
--- books/views/libros_view.py
+++ books/views/libros_view.py
@@ -2,4 +2,49 @@ from django.shortcuts import render
 
 # Create your views here.
 def libros_view(request):
-    return render(request, 'libros/libros_list.html')
+    libros = [
+      {
+          "id": 1,
+          "nombre": "Don Quijote de la Mancha"
+      },
+      {
+          "id": 2,
+          "nombre": "Harry Potter y la piedra Filosofal"
+      },
+      {
+          "id": 3,
+          "nombre": "Clean Code"
+      },
+    ]
+    
+    context = {
+        "libros": libros,
+        "titulo": "Prueba de paso de contexto"
+    }
+    return render(request, 'libros/libros_list.html', context)
+
+# Como está esperando un id para mostrar la lista que corresponde, tiene que pasarse como parámetro.
+def libro_detail(request, id):
+    libros = [
+      {
+          "id": 1,
+          "nombre": "Don Quijote de la Mancha"
+      },
+      {
+          "id": 2,
+          "nombre": "Harry Potter y la piedra Filosofal"
+      },
+      {
+          "id": 3,
+          "nombre": "Clean Code"
+      },
+    ]
+
+    context = {
+        "libro": None,
+    }
+    for libro in libros:
+        if libro['id'] == id:
+            context['libro'] = libro
+
+    return render(request, 'libros/libro_detail.html', context)
