1cbfdfef26a612ae5e665141dfbec1f592c4f308
PabloSanchez <sancheztorrespablo@gmail.com>
2024-07-19 19:36:57 +0200

    Realizamos una prueba de paso de contexto a una vista. Modificamos el html y la view donde tenemos un diccionario y creamos un context que le pasamos al render que se devuelve

--- books/views/autores_view.py
+++ books/views/autores_view.py
@@ -2,4 +2,29 @@ from django.shortcuts import render
 
 # Create your views here.
 def autores_view(request):
-    return render(request, 'autores/autores_list.html')
+    
+        
+    ## Muestra de como pasarle datos a nuestra vista.
+    # Podemos verlo con debug_tools en la pestaña plantilla.
+    autores = [
+      {
+          "id": 1,
+          "nombre": "Antonio"
+      },
+      {
+          "id": 2,
+          "nombre": "Felipe"
+      },
+      {
+          "id": 3,
+          "nombre": "Matilde"
+      },
+    ]
+    
+    context = {
+        "autores": autores,
+        "titulo": "Prueba de paso de contexto"
+    }
+    
+    
+    return render(request, 'autores/autores_list.html', context)
