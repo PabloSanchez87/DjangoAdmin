cf14bdf4e641ef4681060a6086566f96078fa0da
PabloSanchez <sancheztorrespablo@gmail.com>
2024-07-19 20:40:55 +0200

    Trabajamos con parámetros y contexto tambien con editoriales

--- books/urls.py
+++ books/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 # importamos las vistas de autores, libros y editoriales.
 from .views import (
     editoriales_view,
+    editorial_detail,
     autores_view, 
     autor_detail,
     libro_detail,
@@ -16,6 +17,7 @@ app_name="books"
 
 urlpatterns = [
     path("editoriales/", editoriales_view, name="editorial_list"),  
+    path("editoriales/<int:id>/", editorial_detail, name="editorial_detail"),  
    
     path("autores/", autores_view, name="autor_list"),  
     path("autores/<int:id>/", autor_detail, name="autor_detail"),  # Solo machea con autores/# siendo # un número.
--- books/views/editoriales_view.py
+++ books/views/editoriales_view.py
@@ -3,4 +3,50 @@ from django.shortcuts import render
 # Create your views here.
 
 def editoriales_view(request):
-    return render(request, 'editoriales/editoriales_list.html')
+    
+    editoriales = [
+      {
+          "id": 1,
+          "nombre": "Planeta"
+      },
+      {
+          "id": 2,
+          "nombre": "Disney"
+      },
+      {
+          "id": 3,
+          "nombre": "Espasa"
+      },
+    ]
+    
+    context = {
+        "editoriales": editoriales,
+        "titulo": "Prueba de paso de contexto"
+    }
+    
+    return render(request, 'editoriales/editoriales_list.html', context)
+
+def editorial_detail(request, id):
+    editoriales = [
+      {
+          "id": 1,
+          "nombre": "Planeta"
+      },
+      {
+          "id": 2,
+          "nombre": "Disney"
+      },
+      {
+          "id": 3,
+          "nombre": "Espasa"
+      },
+    ]
+
+    context = {
+        "editorial": None,
+    }
+    for editorial in editoriales:
+        if editorial['id'] == id:
+            context['editorial'] = editorial
+
+    return render(request, 'editoriales/editorial_detail.html', context)
