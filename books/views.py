9cffa3d872739dffd0c2d23889665247f124d8ea
PabloSanchez <sancheztorrespablo@gmail.com>
2024-07-19 18:06:34 +0200

    Creamos las vistas para los modelos libros, autores y editoriales. Creamos en views.py de books las funciones para llamar a esas vistas. Creamos carpetas para los archivos html de la vistas separadas por modelos.

--- books/views.py
+++ books/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def autores_view(request):
+    return render(request, 'general/contact.html')
+
+def libros_view(request):
+    return render(request, 'general/contact.html')
+
+def editoriales_view(request):
+    return render(request, 'general/contact.html')
